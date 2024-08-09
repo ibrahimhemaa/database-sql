c6ddf42b2ee6658a8c3198e2d2e498c1335ec2e7
ibrahimhemaa <103036430+ibrahimhemaa@users.noreply.github.com>
2024-08-09 19:55:37 +0300

    Time: 515 ms (35.32%) | Memory: 0B (100.00%) - LeetSync

--- 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 select activity_date as day , count(distinct user_id) as active_users 
 from Activity 
-where date_add(activity_date,interval 30 day) > '2019-07-27' and activity_date<='2019-07-27' 
+where activity_date between date_add('2019-07-27',interval -29 day) and '2019-07-27'
 group by 1;
