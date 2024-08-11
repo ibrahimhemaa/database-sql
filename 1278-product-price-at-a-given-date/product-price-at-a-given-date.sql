1f02ab3da758ae75511e4c814cce7063625712f2
ibrahimhemaa <103036430+ibrahimhemaa@users.noreply.github.com>
2024-08-11 16:21:53 +0300

    Time: 870 ms (92.09%) | Memory: 0B (100.00%) - LeetSync

--- 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -4,6 +4,6 @@ SELECT
     COALESCE(
         (select top 1  p1.new_price from Products p1
         where p.product_id=p1.product_id and p1.change_date<='2019-08-16'
-        order by p1.change_date desc ),10
+        ORDER BY p1.change_date DESC ),10
     ) as price
 FROM (select Distinct product_id from Products) as p;
