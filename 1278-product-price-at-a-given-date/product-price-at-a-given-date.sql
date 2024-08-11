57c38f45039a415ab7b26e617d37254ffa981c6b
ibrahimhemaa <103036430+ibrahimhemaa@users.noreply.github.com>
2024-08-11 16:20:56 +0300

    Time: 1372 ms (13.24%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1278-product-price-at-a-given-date/product-price-at-a-given-date.sql
@@ -0,0 +1,9 @@
+/* Write your T-SQL query statement below */
+SELECT 
+    p.product_id,
+    COALESCE(
+        (select top 1  p1.new_price from Products p1
+        where p.product_id=p1.product_id and p1.change_date<='2019-08-16'
+        order by p1.change_date desc ),10
+    ) as price
+from (select distinct product_id from Products) as p;
