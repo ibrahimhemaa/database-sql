b12d5323e25ed02094c2833d38739ec723cb91c4
ibrahimhemaa <103036430+ibrahimhemaa@users.noreply.github.com>
2024-08-12 07:14:28 +0300

    Time: 12041 ms (78.04%) | Memory: 0B (100.00%) - LeetSync

--- 2057-count-salary-categories/count-salary-categories.sql
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -1,7 +1,7 @@
 /* Write your T-SQL query statement below */
 with ts as ( SELECT 
         SUM(CASE WHEN income < 20000 THEN 1 ELSE 0 END) AS Low,
-        SUM(CASE WHEN income >= 20000 AND income <= 50000 THEN 1 ELSE 0 END) AS Average,
+        SUM(CASE WHEN income  between 20000 AND  50000 THEN 1 ELSE 0 END) AS Average,
         SUM(CASE WHEN income > 50000 THEN 1 ELSE 0 END) AS High
     FROM Accounts )
 select 'Low Salary'  as category,sum(Low)  as accounts_count from ts
