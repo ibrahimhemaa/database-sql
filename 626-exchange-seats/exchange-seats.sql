ce7f689ebfb9f2bfa6088c43fb3aa044a3b5b99a
ibrahimhemaa <103036430+ibrahimhemaa@users.noreply.github.com>
2024-08-12 08:08:02 +0300

    Time: 516 ms (12.44%) | Memory: 0B (100.00%) - LeetSync

--- 626-exchange-seats/exchange-seats.sql
+++ 626-exchange-seats/exchange-seats.sql
@@ -1,6 +1,7 @@
 /* Write your T-SQL query statement below */
+with mx as(select max(id) as mxs from Seat)
 select case
-    when id%2=1 and id+1 not in(select id from Seat)then id
+    when id%2=1 and id=mx.mxs then id
     when id%2=0 then id-1
     else id+1 end as id , student
-from Seat order by id; 
+from Seat,mx order by id; 
